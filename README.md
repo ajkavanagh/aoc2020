6c55dcb46ec7ff35f2bea9c6f782ece33598e993
Alex Kavanagh <alex@ajkavanagh.co.uk>
2020-12-09 20:56:16 +0000

    Add initial day handling code and very early framework

---
+++ README.md
@@ -0,0 +1,21 @@
+# Advent of Code 2020
+
+The is the start of my AoC 2020.  It's in the Rust language as a way to
+kickstart using Rust more frequently.  Rust joins Python, Bash and Haskell as
+my current "in use" languages.
+
+# Building
+
+aoc202 needs a rust toolchain to build it.
+[rustup.rs](https://rustup.rs/) is probably the easiest way to get it.
+The stable channel is the one to use.
+
+## Steps
+
+1. Clone the repository.
+2. `cargo build --release`
+3. run it with a day-part number (e.g. 1-2 is day 1, part 2)
+
+# Using
+
+TBA
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::cmp;
+use std::process;
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::fs;
+
+
+struct Config {
+    day: u32,
+    part: u32,
+}
+
+
+const MAX_DAY: u32 = 1;  // update when we add a day
+
+impl Config {
+
+    fn new(args: &[String]) -> Result<Config, String> {
+        let num_args = args.len();
+        if num_args == 1 {
+            return Ok(Config {day: 1, part: 1})
+        }
+        let command = args[1].to_lowercase();
+        let parts = command.split("-").into_iter().collect::<Vec<&str>>();
+        if parts.len() != 2 {
+            return Err(format!("command '{}' isn't a valid day-part", command));
+        }
+        let day: u32 = parts[0].parse().unwrap_or(0);
+        let part: u32 = parts[1].parse().unwrap_or(0);
+        if day < 1 || day > MAX_DAY || part < 1 || part > 2 {
+            return Err(format!("day or part is not parsable as an int or in range: input was '{}'", command));
+        }
+        return Ok(Config{day, part});
+    }
+}
+
+
+
+fn usage() -> Result<(), String> {
+    eprintln!("Usage: aoc2020 <day>-<part>");
+    Ok(())
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Couldn't parse arguments: {}", err);
+        usage().unwrap();
+        process::exit(1);
+    });
+    println!("the day is {}-{}", config.day, config.part);
+}
